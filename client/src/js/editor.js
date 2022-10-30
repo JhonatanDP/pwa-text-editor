a55b6440d547f437b6937d562a502aff8e005969
Jhonatan <jonathandiazp@gmail.com>
2022-10-30 17:46:27 -0400

    db and editor

---
+++ client/src/js/editor.js
@@ -0,0 +1,25 @@
+import { getDb, putDb } from './database';
+import { header } from './header';
+
+export default class {
+  constructor() {
+    const localData = localStorage.getItem('content');
+    if (typeof CodeMirror === 'undefined') {
+      throw new Error('CodeMirror is not loaded');
+    }
+
+    this.editor = CodeMirror(document.querySelector('#main'), {
+      value: '',
+      mode: 'javascript',
+      theme: 'monokai',
+      lineNumbers: true,
+      lineWrapping: true,
+      autofocus: true,
+      indentUnit: 2,
+      tabSize: 2,
+    });
+
+
+
+  }
+}
